b6cc6edc4554c97586988256c3f1d154c6f5220f
bitfl0wer <florian@pro-weber.com>
2023-09-26 14:33:44 +0200

    Multiple element test

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello yew and trunk!" }</h1>
+        <>
+            <h1>{ "Hello yew and trunk!" }</h1>
+            <p>{"This is some text."}</p>
+        </>
     }
 }
 
