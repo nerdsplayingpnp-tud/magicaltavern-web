0f8c29287d2b21e26aa2d907452d7b0d8110faf9
bitfl0wer <florian@pro-weber.com>
2023-09-26 14:14:31 +0200

    Hello yew and trunk!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <h1>{ "Hello yew and trunk!" }</h1>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
